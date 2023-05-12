d64e13dd480910ff909442a7122ecf09c0ec0436
Mustapha Alahji Ibrahim <iamustapha213@gmail.com>
2023-05-12 10:33:59 +0100

    mm

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'a3ba3bfe5bee569cfc2859f1b54d3bac6baf1d7d',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'a3ba3bfe5bee569cfc2859f1b54d3bac6baf1d7d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
