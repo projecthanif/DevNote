ecff8f80d9df007c27432d4fe5059657adddcde1
projecthanif <iamustpha213@gmail.com>
2023-08-07 08:52:46 +0100

    dome

--- views/partial/head.php
+++ views/partial/head.php
@@ -8,7 +8,7 @@
     <meta name="Description" content="Enter your description here" />
     <link rel="stylesheet" href="/views/bootstrap/dist/css/bootstrap.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js"></script> -->
     <!-- <link rel="stylesheet" href="/views/free/css/all.css"> -->
     <link rel="stylesheet" href="/views/assets/css/style.css">
     <link rel="icon" type="x/icon" href="/views/assets/image/cc-primary.png">
