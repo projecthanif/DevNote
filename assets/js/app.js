c8635a1e2d88f7b5578a5538e54328c4710e71e9
projecthanif <iamustpha213@gmail.com>
2023-08-12 16:27:22 +0100

    done

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,7 +7,7 @@ function showNav() {
     aside.style.display = 'block';
     aside.style.width = '100%'
     aside.style.height = '100%'
-    aside.style.backgroundColor = 'grey'
+    aside.style.backgroundColor = 'white'
     aside.style.zIndex = 1;
     document.getElementById('close').style.display = 'inline'
     document.getElementById('header').style.zIndex = -1;
