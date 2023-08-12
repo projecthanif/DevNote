04aba449ad2b6d2a65442933a4398de485e2c9d6
projecthanif <iamustpha213@gmail.com>
2023-08-12 14:32:33 +0100

    navbar

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,19 @@
+let toggle = document.getElementById("toggle");
+let aside  = document.getElementById('nav-column');
 
-let header = document.getElementById("#header");
 
+
+function showNav() {
+    aside.style.display = 'block';
+    aside.style.width = '100%'
+    aside.style.height = '100%'
+    aside.style.backgroundColor = 'grey'
+    aside.style.zIndex = 1;
+    document.getElementById('close').style.display = 'inline'
+    document.getElementById('header').style.zIndex = -1;
+    console.log('Hello World');
+}
+
+function closeNav() {
+    aside.style.display = 'none';
+}
